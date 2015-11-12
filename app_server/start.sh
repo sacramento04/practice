30a56cafa98e768e8d9c479f469aa4a95b0d70e9
sacramento04 <sacramento04@163.com>
2015-11-12 11:26:26 +0800

    modify

--- app_server/start.sh
+++ app_server/start.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-erl +P 102400 +K true -boot start_sasl -pa ebin -s app_server start -name jarvis_server@192.168.24.89 -setcookie jarvis -env ERL_MAX_ETS_TABLES 100000
+IPADDR=`LC_ALL=C ifconfig  | grep 'inet addr:'| grep -v '127.0.0.1' | cut -d: -f2 | awk '{ print $1}'`
+erl +P 102400 +K true -boot start_sasl -pa ebin -s app_server start -name app_server@${IPADDR} -setcookie jarvis -env ERL_MAX_ETS_TABLES 100000
